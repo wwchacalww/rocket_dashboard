aeb7326107dd67b35efddb0050386088ee3b3f25
Vicente Cartaxo <juniorcartaxo@gmail.com>
2022-06-28 10:04:43 -0300

    Componente Pagination criado

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -15,6 +15,7 @@ import {
 } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
+import Pagination from "../../components/Pagination";
 import Sidebar from "../../components/Sidebar";
 
 export default function Userlist() {
@@ -76,6 +77,8 @@ export default function Userlist() {
               </Tr>
             </Tbody>
           </Table>
+
+          <Pagination />
         </Box>
       </Flex>
     </Box>
