d95e3c7535f0bfdcf5904e420772ca5529880fd3
Vicente Cartaxo <juniorcartaxo@gmail.com>
2022-06-29 11:46:16 -0300

    ⚒ Componentes e paginas mais responsivos agora

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,20 +1,25 @@
 import { Flex, Box, Text, Avatar } from "@chakra-ui/react";
 
-export default function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+export default function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
+      {showProfileData && (
         <Box mr="4" textAlign="right">
           <Text>Hakuna Matata</Text>
           <Text color="gray.300" fontSize="small">
             hakuna@matata.com
           </Text>
         </Box>
+      )}
 
-        <Avatar
-          size="md"
-          name="Hakuna Matata"
-          src="https://github.com/wwchacalww.png"
-        />
-      </Flex>
+      <Avatar
+        size="md"
+        name="Hakuna Matata"
+        src="https://github.com/wwchacalww.png"
+      />
+    </Flex>
   );
 }
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -12,13 +12,19 @@ import {
   Thead,
   Tr,
   Text,
+  useBreakpointValue,
 } from "@chakra-ui/react";
+import Link from "next/link";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import Pagination from "../../components/Pagination";
 import Sidebar from "../../components/Sidebar";
 
 export default function Userlist() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
   return (
     <Box>
       <Header />
@@ -29,31 +35,33 @@ export default function Userlist() {
             <Heading size="lg" fontWeight="normal">
               Usuários
             </Heading>
-            <Button
-              as="a"
-              size="sm"
-              fontSize="sm"
-              colorScheme="pink"
-              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
-            >
-              Criar novo
-            </Button>
+            <Link href="/users/create" passHref>
+              <Button
+                as="a"
+                size="sm"
+                fontSize="sm"
+                colorScheme="pink"
+                leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+              >
+                Criar novo
+              </Button>
+            </Link>
           </Flex>
 
           <Table colorScheme="whiteAlpha">
             <Thead>
               <Tr>
-                <Th px="6" color="gray.300" w="8">
+                <Th px={["4", "4", "6"]} color="gray.300" w="8">
                   <Checkbox colorScheme="pink" />
                 </Th>
                 <Th>Usuário</Th>
-                <Th>Data de Cadastro</Th>
+                {isWideVersion && <Th>Data de Cadastro</Th>}
                 <Th width="8"></Th>
               </Tr>
             </Thead>
             <Tbody>
               <Tr>
-                <Td px="6">
+                <Td px={["4", "4", "6"]}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
@@ -64,18 +72,20 @@ export default function Userlist() {
                     </Text>
                   </Box>
                 </Td>
-                <Td>28 de junho, 2022</Td>
-                <Td>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                  >
-                    Editar
-                  </Button>
-                </Td>
+                {isWideVersion && <Td>28 de junho, 2022</Td>}
+                {isWideVersion && (
+                  <Td>
+                    <Button
+                      as="a"
+                      size="sm"
+                      fontSize="sm"
+                      colorScheme="purple"
+                      leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                    >
+                      Editar
+                    </Button>
+                  </Td>
+                )}
               </Tr>
             </Tbody>
           </Table>
